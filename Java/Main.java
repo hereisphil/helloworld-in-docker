67500637217b002a0ead4061fd445dad6a5e4805
hereisphil <thereisphil@gmail.com>
2026-01-06 12:16:46 -0600

    Java done

---
+++ Java/Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+// Followed this YouTube Video:
+// https://www.youtube.com/watch?v=6cp4P4XZ9hE
+class Main {
+  public static void main(String[] args){
+    System.out.println("Hello ASL!");
+
+    Date currentDate = new Date();
+    System.out.println("Proccess completed: " + currentDate);
+  }
+}
